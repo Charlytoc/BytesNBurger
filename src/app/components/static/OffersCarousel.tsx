ba9aff6c8175f64946ad2908b1aa503c63284350
Charlytoc <charlyjchaconc@gmail.com>
2023-10-19 15:58:01 -0500

    all with next and yastá

--- src/app/components/static/OffersCarousel.tsx
+++ src/app/components/static/OffersCarousel.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function OffersCarousel () {
     return <>
             <div className="carousel-component">
-                <CarouselItem name="Mega Burger" imgAlt="Mega burger" imgSrc="https://res.cloudinary.com/charlycloud/image/upload/v1697747413/w0sootlaukl88xh62s5g.jpg" productDetails="Esta es nuestra hamburguesa estrella, hecha a partir de blablabla"/>
+                <CarouselItem name="Mega Burger" imgAlt="Mega burger" imgSrc="/products/bytesnburger.jpg" productDetails="Esta es nuestra hamburguesa estrella, hecha a partir de blablabla"/>
             </div>
         </>
 }
